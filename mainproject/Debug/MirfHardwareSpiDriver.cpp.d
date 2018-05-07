ecbd4af6d1cf5a6a1af58db7f39b74c6a0b3f80a
FishYu-CN <38803378+FishYu-CN@users.noreply.github.com>
2018-05-07 17:26:25 +0800

    ver2

--- mainproject/Debug/MirfHardwareSpiDriver.cpp.d
+++ mainproject/Debug/MirfHardwareSpiDriver.cpp.d
@@ -2,15 +2,15 @@ C:\Users\Fish\AppData\Local\Temp\VMBuilds\mainproject\uno\Debug\MirfHardwareSpiD
  C:\Users\Fish\Documents\GitHub\Arduino-nRF24L01\Mirf\MirfHardwareSpiDriver.cpp \
  C:\Users\Fish\Documents\GitHub\Arduino-nRF24L01\Mirf\MirfHardwareSpiDriver.h \
  C:\Users\Fish\Documents\GitHub\Arduino-nRF24L01\Mirf\MirfSpiDriver.h \
- C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\libraries\SPI\src/SPI.h \
- C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino/binary.h \
- C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino/WCharacter.h \
- C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino/WString.h \
- C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
- C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino/Stream.h \
- C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino/Print.h \
- C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino/Printable.h \
- C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino/USBAPI.h \
- C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\variants\standard/pins_arduino.h
+ D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\libraries\SPI\src/SPI.h \
+ D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/binary.h \
+ D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/WString.h \
+ D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Stream.h \
+ D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Print.h \
+ D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Printable.h \
+ D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\variants\standard/pins_arduino.h
