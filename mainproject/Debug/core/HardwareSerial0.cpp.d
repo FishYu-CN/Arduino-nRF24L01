237b3b4244f476fe5b1c31808c57b29ca168bdad
FishYu-CN <38803378+FishYu-CN@users.noreply.github.com>
2018-05-07 17:22:09 +0800

    test

--- mainproject/Debug/core/HardwareSerial0.cpp.d
+++ mainproject/Debug/core/HardwareSerial0.cpp.d
@@ -1,14 +1,14 @@
 C:\Users\Fish\AppData\Local\Temp\VMBuilds\mainproject\uno\Debug\core\HardwareSerial0.cpp.o: \
- D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\HardwareSerial0.cpp \
- D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Arduino.h \
- D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\binary.h \
- D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\WCharacter.h \
- D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\WString.h \
- D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\HardwareSerial.h \
- D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Stream.h \
- D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Print.h \
- D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Printable.h \
- D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\USBAPI.h \
- D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\variants\standard/pins_arduino.h \
- D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\HardwareSerial_private.h \
- D:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\wiring_private.h
+ C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino\HardwareSerial0.cpp \
+ C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino\Arduino.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino\binary.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino\WCharacter.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino\WString.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino\HardwareSerial.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino\Stream.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino\Print.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino\Printable.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino\USBAPI.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino\HardwareSerial_private.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.2\hardware\arduino\avr\cores\arduino\wiring_private.h
--- mainproject/mainproject.ino
+++ mainproject/mainproject.ino
@@ -22,7 +22,8 @@
 #include "MirfHardwareSpiDriver.h"
 
 unsigned char TADDR[5] = { 0x34,0x43,0x10,0x10,0x01 };
-uint8_t sendbuf[32];
+unsigned long time;
+uint8_t sendbuf[32], recvbuf[32];
 void setup() {
 	Serial.begin(9600);
 	/*
@@ -34,7 +35,7 @@ void setup() {
 	* Mirf.csnPin = 9;
 	* Mirf.cePin = 7;
 	*/
-	
+
 	Mirf.cePin = 8;
 	Mirf.csnPin = 7;
 	Mirf.payload = 32;
@@ -72,20 +73,20 @@ void setup() {
 	//Serial.println(buf[0], HEX);
 
 	Mirf.configRegister(SETUP_RETR, 0x1a);//设置自动重发间隔时间:500us + 86us;最大自动重发次数:10次
-	//Mirf.readRegister(SETUP_RETR, buf, 1);
-	//Serial.println(buf[0], HEX);
+										  //Mirf.readRegister(SETUP_RETR, buf, 1);
+										  //Serial.println(buf[0], HEX);
 
-	//Mirf.configRegister(RF_CH, 0);
-	//Mirf.readRegister(RF_CH, buf, 1);
-	//Serial.println(buf[0], HEX);
+										  //Mirf.configRegister(RF_CH, 0);
+										  //Mirf.readRegister(RF_CH, buf, 1);
+										  //Serial.println(buf[0], HEX);
 
-	//Mirf.configRegister(RF_SETUP, 0x0f);
-	//Mirf.readRegister(RF_SETUP, buf, 1);
-	//Serial.println(buf[0], HEX);
+										  //Mirf.configRegister(RF_SETUP, 0x0f);
+										  //Mirf.readRegister(RF_SETUP, buf, 1);
+										  //Serial.println(buf[0], HEX);
 
-	//Mirf.configRegister(CONFIG, 0x0e);
-	//Mirf.readRegister(CONFIG, buf, 1);
-	//Serial.println(buf[0], HEX);
+										  //Mirf.configRegister(CONFIG, 0x0e);
+										  //Mirf.readRegister(CONFIG, buf, 1);
+										  //Serial.println(buf[0], HEX);
 
 	Mirf.ceHi();
 
@@ -111,21 +112,78 @@ void setup() {
 	//Serial.println(buf[0], HEX);
 	//Mirf.ceLow();
 
-	sendbuf[0] = 1;
-	sendbuf[1] = 'a';
-
+	sendbuf[0] = 6;
+	sendbuf[1] = 'C';
+	sendbuf[2] = '1';
+	sendbuf[3] = ':';
+	sendbuf[4] = '0';
+	sendbuf[5] = '5';
+	sendbuf[6] = '0';
 }
 
 void loop() {
+
+	unsigned int raw;
 	Mirf.setTADDR(TADDR);
+	raw = analogRead(A0);
 
-
+	raw = map(raw, 0, 1024, 0, 100);
+	Serial.println(raw);
+	sendbuf[4] = (raw / 100) + '0';
+	sendbuf[5] = (raw / 10) + '0';
+	sendbuf[6] = (raw % 10) + '0';
 	Mirf.send(sendbuf);
 	while (Mirf.isSending()) {
 	}
+
 	Serial.println("Finished sending");
-	sendbuf[1]=='z'?sendbuf[1] = 'a': sendbuf[1]++;
-	
+	//sendbuf[1]=='z'?sendbuf[1] = 'a': sendbuf[1]++;
+	time = millis();
+	while (!Mirf.dataReady())
+	{
+		if ((millis() - time) > 1000)
+			break;
+	}
+	if (!Mirf.isSending() && Mirf.dataReady()) {
+		Serial.println("Got packet");
+
+		/*
+		* Get load the packet into the buffer.
+		*/
+
+		Mirf.getData(recvbuf);
+
+		if (recvbuf[2] == '1')
+		{
+			if (recvbuf[4] == 'O')
+				digitalWrite(2, LOW);
+			else
+				digitalWrite(2, HIGH);
+		}
+
+		for (int i = 0;i<recvbuf[0];i++)
+			Serial.print((char)recvbuf[i + 1]);
+		/*
+		* Set the send address.
+		*/
+
+
+		//Mirf.setTADDR(RADDR);
+
+		/*
+		* Send the data back to the client.
+		*/
+
+		//Mirf.send(data);
+
+		/*
+		* Wait untill sending has finished
+		*
+		* NB: isSending returns the chip to receving after returning true.
+		*/
+
+		//Serial.println("Reply sent.");
+	}
 	delay(1000);
 }
 
